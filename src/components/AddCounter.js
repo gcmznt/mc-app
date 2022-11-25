d5e2f34c72e9179e9d2c8a496422e1504549ecad
Giacomo Zinetti <giacomo.zinetti@appway.com>
2022-11-25 15:32:06 +0100

    fix styles

--- src/components/AddCounter.js
+++ src/components/AddCounter.js
@@ -111,6 +111,7 @@ export default function AddCounter({
                   onChange={() => setCustomType(type)}
                   type="radio"
                   value={customType}
+                  variant="inline"
                 />
               ))}
               <footer>
